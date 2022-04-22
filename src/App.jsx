1476a0877478adfd665c1c285a846eaff3d9404a
saanjay007 <007saanjaypatil@gmail.com>
2022-04-22 14:29:35 +0530

    s
    s

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Phaser from "phaser";
+import Boot from "./scenes/Boot.js";
+import Preloader from "./scenes/Preloader.js";
+import MainMenu from "./scenes/MainMenu.js";
+import MainGame from "./scenes/Game.js";
+import {useState} from "react";
+
+
+let game = null;
+
+function App() {
+
+   const [loaded, setloaded] = useState(false);
+
+  return 
+    <></>
+  
+}
+
+export default App;
